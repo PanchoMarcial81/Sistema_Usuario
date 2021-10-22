8c738a7fe89f6604ad7bb02959affb0fd66d5ba2
pmarcial <espinoza.pancho@gmail.com>
2021-10-22 13:39:01 -0300

    Organizar Header y Footer

---
+++ footer.php
@@ -0,0 +1,11 @@
+	<!-- FOOTER -->
+	<footer class="black">
+		<div class="container">
+			<p class="copy">
+				&copy; Todos los derechos reservados - Sistema de Usuarios
+			</p>
+		</div>
+	</footer>
+	<script src="js/app.js"></script>
+</body>
+</html>
